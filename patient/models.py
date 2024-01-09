5f8e38001194194cc4417825b65525e17069ae95
Priyanshu Sharma <ps4798214@gmail.com>
2024-01-10 03:10:41 +0530

    add basic user/patient/doctor model

--- patient/models.py
+++ patient/models.py
@@ -1,2 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    pass
+
+class Patient(models.Model):
+    
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    doctor = models.ForeignKey("Doctor", on_delete=models.CASCADE)
+    
+class Doctor(models.Model):
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)    
